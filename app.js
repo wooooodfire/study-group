23a24d0b46512647e1b799c0b82f9c2dc80cac52
wooooodfire <jeffrey98064@gmail.com>
2022-06-24 23:20:59 +0800

    update web

---
+++ app.js
@@ -0,0 +1,5 @@
+/* Open when someone clicks on the span element */
+const openNav = () => (document.getElementById('nav-mobile').style.width = '100%');
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+const closeNav = () => (document.getElementById('nav-mobile').style.width = '0%');
