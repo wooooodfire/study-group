6de8e530ac7fd98c3178c1da4254fc767b95674e
wooooodfire <jeffrey98064@gmail.com>
2022-06-25 00:27:17 +0800

    final

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ const navClick = (index) => {
 	title[index].scrollIntoView();
 	document.getElementById('nav-mobile').style.width = '0%';
 };
+
+// TOP BUTTON
+
+//Get the button:
+mybutton = document.getElementById('toTop');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => scrollFunction();
+
+const scrollFunction = () => {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+};
+
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
