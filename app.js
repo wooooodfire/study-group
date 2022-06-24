551fc3725964b1493c4b1309d5cb88324d32f9c7
wooooodfire <jeffrey98064@gmail.com>
2022-06-25 00:12:45 +0800

    update web

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 /* Open when someone clicks on the span element */
-const openNav = () => (document.getElementById('nav-mobile').style.width = '100%');
+const openNav = () => {
+	document.getElementById('nav-mobile').style.width = '100%';
+};
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-const closeNav = () => (document.getElementById('nav-mobile').style.width = '0%');
+const closeNav = () => {
+	document.getElementById('nav-mobile').style.width = '0%';
+};
+
+const title = document.getElementsByClassName('content-block-title');
+
+const navClick = (index) => {
+	title[index].scrollIntoView();
+	document.getElementById('nav-mobile').style.width = '0%';
+};
